43ad0b6d9ce3384bf15ede9479c1bf1ab2d88ab8
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-01-15 19:29:12 +0200

    Created base.html

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+# import picamera
+import cv2
+import socket
+import io
+
 app = Flask(__name__)
+vc = cv2.VideoCapture(0)
+
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+
+def gen():
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=81, host='0.0.0.0')
+    app.run(debug=True, port=81, host='0.0.0.0', threaded=True)
