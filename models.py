cbfc202f9dcaefe7f67e11f254fa21ab95b698ec
NikiDeyanov86 <33031929+NikiDeyanov86@users.noreply.github.com>
2022-02-12 18:24:51 +0200

    Added user model

---
+++ models.py
@@ -0,0 +1,12 @@
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
