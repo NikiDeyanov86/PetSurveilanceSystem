4b572983bc9a468b6231c754a49721dff35d41fa
NikiDeyanov86 <33031929+NikiDeyanov86@users.noreply.github.com>
2022-02-12 21:19:49 +0200

    Fixxed circular import in models

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
-from app import db
+# from app import db
+from sqlalchemy import Column, Integer, String
 from flask_login import UserMixin
 
 
 class User(UserMixin, db.Model):
     __tablename__ = "User"
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
     # login_id = db.Column(db.String(36), nullable=True)
 
     def __init__(self, **kwargs):
