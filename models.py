f5ae9891458f1675f34e19567c15f7ff983fdfb0
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-02-13 21:55:09 +0200

    Fixing imports

--- models.py
+++ models.py
@@ -11,9 +11,9 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     __tablename__ = "User"
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
     # login_id = db.Column(db.String(36), nullable=True)
 
     def __init__(self, **kwargs):
