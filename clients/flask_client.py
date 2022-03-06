7605aace57885e7daa5b86bec2d9bcfc06bd8e6f
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-03-06 13:49:59 +0200

    Fixing name error

--- clients/flask_client.py
+++ clients/flask_client.py
@@ -15,47 +15,42 @@ class Check:
     mov_available = None
 
 
-global flask_client
-
-
-def on_connect(client, userdata, flags, rc):
-    print("Client connected to broker with response code ", rc)
-    flask_client.subscribe(topic_feedback)
-
-
-def on_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    # print("Received message: ", data['payload'])
-    # app.logger.info('Received message')
-
-    if data['payload'] == "object_visible":
-        Check.visible = True
-
-    elif data['payload'] == "object_lost":
-        Check.visible = False
-
-    elif data['payload'] == "hl_connected":
-        Check.hl_available = True
-
-    elif data['payload'] == "hl_disconnected":
-        Check.hl_available = False
-
-    elif data['payload'] == "mov_connected":
-        Check.mov_available = True
-
-    elif data['payload'] == "mov_disconnected":
-        Check.mov_available = False
-
-
-def on_publish(client, userdata, result):
-    print("Published to broker")
-    pass
-
-
 def init_mqtt():
+
+    def on_connect(client, userdata, flags, rc):
+        print("Client connected to broker with response code ", rc)
+        flask_client.subscribe(topic_feedback)
+
+    def on_message(client, userdata, message):
+        data = dict(
+            topic=message.topic,
+            payload=message.payload.decode()
+        )
+        # print("Received message: ", data['payload'])
+        # app.logger.info('Received message')
+
+        if data['payload'] == "object_visible":
+            Check.visible = True
+
+        elif data['payload'] == "object_lost":
+            Check.visible = False
+
+        elif data['payload'] == "hl_connected":
+            Check.hl_available = True
+
+        elif data['payload'] == "hl_disconnected":
+            Check.hl_available = False
+
+        elif data['payload'] == "mov_connected":
+            Check.mov_available = True
+
+        elif data['payload'] == "mov_disconnected":
+            Check.mov_available = False
+
+    def on_publish(client, userdata, result):
+        print("Published to broker")
+        pass
+
     flask_client = mqtt.Client("Flask")
     # client.username_pw_set(username, password)
     flask_client.on_connect = on_connect
