52317e9ef1181d46a547b136175fc3094a1b02c0
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-04-07 09:43:33 +0300

    Testing relay switch with pin 18

--- test_relay.py
+++ test_relay.py
@@ -7,13 +7,13 @@ GPIO.setup(18, GPIO.OUT)
 print("Pin set")
 
 print("Setting to low")
-GPIO.output(18, GPIO.LOW)
+GPIO.output(18, False)
 print("Set to low")
 
 time.sleep(5)
 
 print("Setting to HIGH")
-GPIO.output(18, GPIO.HIGH)
+GPIO.output(18, True)
 print("Set to high")
 time.sleep(2)
 GPIO.cleanup()
