2a3a80a991cdd089b6589ff82597b279b3ffdbac
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-04-07 09:23:03 +0300

    Testing relay switch with pin 17

--- test_relay.py
+++ test_relay.py
@@ -4,9 +4,12 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+print("Pin set")
 
 time.sleep(5)
 
+print("Setting to HIGH")
 GPIO.output(17, GPIO.HIGH)
+print("Set to high")
 time.sleep(2)
 GPIO.cleanup()
