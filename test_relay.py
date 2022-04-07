60135eb53693ea620ca2187734c1f6538ba42614
NikiDeyanov86 <deyanov.nikolay@gmail.com>
2022-04-07 09:25:10 +0300

    Testing relay switch with pin 18

--- test_relay.py
+++ test_relay.py
@@ -3,13 +3,17 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 print("Pin set")
+
+print("Setting to low")
+GPIO.output(18, GPIO.LOW)
+print("Set to low")
 
 time.sleep(5)
 
 print("Setting to HIGH")
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(18, GPIO.HIGH)
 print("Set to high")
 time.sleep(2)
 GPIO.cleanup()
